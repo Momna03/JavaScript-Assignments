ed94df3dbd6b16ec96b56bd6d60908c18584eb4c
Momna03 <momina.javaid03@gmail.com>
2021-01-10 20:30:37 +0500

    Update task4 code

--- Assignment3_PIAIC72845/task4.js
+++ Assignment3_PIAIC72845/task4.js
@@ -1,8 +1,10 @@
 cities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
 selectedCities = [];
 
-document.write("<h5>" + Cities List: + "</h5>");
+document.write("<h4>Cities List: </h4>");
+document.write("<p>" + cities + "</p>");
 
 // slice(startIndex, endIndex);
-cities.slice(2, 4);
-document.write("<p>" + Selected Cities List: +"</p>");
+selectedCities = cities.slice(2, 4);
+document.write("<h4> Selected Cities List: </h4>");
+document.write("<p>" + selectedCities + "</p>");
