2dfb294277cf47faa4bac19e53e3f25a7636dc02
Momna03 <momina.javaid03@gmail.com>
2021-01-10 20:11:44 +0500

    Task4 completed

---
+++ Assignment3_PIAIC72845/task4.js
@@ -0,0 +1,8 @@
+cities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
+selectedCities = [];
+
+document.write("<h5>" + Cities List: + "</h5>");
+
+// slice(startIndex, endIndex);
+cities.slice(2, 4);
+document.write("<p>" + Selected Cities List: +"</p>");
