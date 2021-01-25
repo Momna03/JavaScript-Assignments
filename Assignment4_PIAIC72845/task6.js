89486872aaefe4bfad60a765114393344ee81e36
Momna03 <momina.javaid03@gmail.com>
2021-01-25 20:12:46 +0500

    Task5 completed

---
+++ Assignment4_PIAIC72845/task6.js
@@ -0,0 +1,5 @@
+var diceValue = Math.round(Math.random()*5 )+ 1;
+document.write("<p>random dice value: " + diceValue + "</p>");
+
+diceValue = Math.round(Math.random()*5) + 1;
+document.write("<p>random dice value: " + diceValue + "</p>");
