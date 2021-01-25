aafb317ffa518416ae468593fd778d3a75aa946d
Momna03 <momina.javaid03@gmail.com>
2021-01-25 19:06:45 +0500

    Task2 completed

---
+++ Assignment4_PIAIC72845/task2.js
@@ -0,0 +1,9 @@
+value = "472";
+
+document.write("<p>Value: " + value + "</p>");
+document.write("<p>Type: " + typeof(value) + "</p>");
+
+value = Number(value);
+
+document.write("<p>Value: " + value + "</p>");
+document.write("<p>Type: " + typeof(value) + "</p>");
