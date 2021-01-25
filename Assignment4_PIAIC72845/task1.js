aa609048c4ac4e77990030455a74961fed504eaf
Momna03 <momina.javaid03@gmail.com>
2021-01-25 18:55:33 +0500

    Task1 completed

---
+++ Assignment4_PIAIC72845/task1.js
@@ -0,0 +1,6 @@
+city = "Hyderabad";
+
+document.write("<p>City: " + city + "</p>");
+
+replacedCity = city.replace("Hyder", "Islam");
+document.write("<p>After replacement: " + replacedCity + "</p>");
